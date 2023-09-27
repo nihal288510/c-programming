da0281d567abfad60911d6843dbe871650e97aa4
nihal288510 <143987672+nihal288510@users.noreply.github.com>
2023-09-28 01:47:41 +0530

    Add files via upload

---
+++ void main().dart
@@ -0,0 +1,17 @@
+void main()
+{
+    oddeven();
+}void oddeven();
+{
+    var c=;
+    int a=5;
+    if(a%2==0)
+    {
+        c='even';
+
+    }
+    else{
+        c='odd';
+    }
+    print(c);
+}
